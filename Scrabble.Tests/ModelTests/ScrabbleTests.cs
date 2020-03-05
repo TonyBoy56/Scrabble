dd76fbfd60e37181d54b7af95e92e4c4722c3719
Student <example@example.com>
2020-03-05 14:18:46 -0800

    add second test

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -8,13 +8,20 @@ namespace Scrabble.TestTools
   [TestClass]
   public class WordTests
   {
+    // [TestMethod]
+    // public void Word_TurnToCharArray_WordCharArray()
+    // {
+    //   char[] expected = new char[] { 'c','a','t' };
+    //   Word newWord = new Word("cat");
+    //   char[] result = newWord.BreakToArray();
+    //   CollectionAssert.AreEqual(expected, result);
+    // }
     [TestMethod]
-    public void Word_TurnToCharArray_WordCharArray()
+    public void Word_ReturnValueOfWord_Number()
     {
-      char[] expected = new char[] { 'c','a','t' };
-      Word newWord = new Word("cat");
-      char[] result = newWord.BreakToArray();
-      CollectionAssert.AreEqual(expected, result);
+      Word newWord = new Word("net");
+      int result = newWord.ReturnValue();
+      CollectionAssert.AreEqual(3, result);
     }
   } 
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Scrabble.Models
 {
@@ -12,20 +13,36 @@ namespace Scrabble.Models
     public Word(string aWord)
     {
       MyWord = aWord;
-    letters.Add( 1, new char[]{'a', 'e','i','o','u','l', 'n','r','s','t'} );
-    letters.Add( 2, new char[]{'d','g'} );
-    letters.Add( 3, new char[]{'b','c','m','p'} );
-    letters.Add( 4, new char[]{'f','h','v','w','y'} );
-    letters.Add( 5, new char[]{'k'} );
-    letters.Add( 8, new char[]{'j','x'} );
-    letters.Add( 10, new char[]{'q','z'} );
+    letters.Add(1, new char[]{'a', 'e','i','o','u','l', 'n','r','s','t'});
+    letters.Add(2, new char[]{'d','g'});
+    letters.Add(3, new char[]{'b','c','m','p'});
+    letters.Add(4, new char[]{'f','h','v','w','y'});
+    letters.Add(5, new char[]{'k'});
+    letters.Add(8, new char[]{'j','x'});
+    letters.Add(10, new char[]{'q','z'});
     }
     
-    public char[] BreakToArray()
+    // public static char[] BreakToArray()
+    // {
+    //   // int wordValue = 0;
+    //   char[] inputWord = MyWord.ToCharArray();
+    //   return inputWord;
+    // }
+
+    public int ReturnValue()
     {
-      // int wordValue = 0;
-      char[] inputWord = MyWord.ToCharArray();
-      return inputWord;
-     }
+      int totalScore = 0;
+      Word inputWord = new Word("a");
+      char[] result = MyWord.ToCharArray();
+      for (int i = 0; i < result.Length; i++);
+      {
+        
+        if (letters.ContainsKey(element))
+        {
+         totalScore +=  
+        }
+      }
+      return totalScore;
+    }
   }
-}
+} 
